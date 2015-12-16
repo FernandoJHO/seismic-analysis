26238bb4b728b90108385d1f0d8cb34e600336eb
FernandoJHO <fernando.handal@yahoo.com>
2015-12-16 20:57:20 -0300

    modificaciones

--- src/sismograma.py
+++ src/sismograma.py
@@ -7,35 +7,65 @@ def obtenerDatos(nombre_archivo):
     datos = np.loadtxt(nombre_archivo)
     return datos
 	
-
-def mostrarGraficoAmp(datos_sismograma):
-    plt.xlabel("Tiempo")
-    plt.ylabel("Amplitud")
-    print("Generando gr�fico...")
-    plt.plot(datos_sismograma)
-    print("Gr�fico generado.")
-    plt.show()
+def obtenerRuido(datos):
+    datos_ruido = []
+    for i in range(0,101):
+        datos_ruido.append(datos[i])
+    return datos_ruido
 	
-def aplicarTransformada(datos_sismo):
+def espectroFrecuencia(datos_sismo):
     datos_trans = np.fft.fft(datos_sismo).real
     return datos_trans
 
-def mostrarGraficoFrec(datos_sismograma):
+def mostrarGrafico(datos_sismograma):
     print("Generando gráfico...")
     plt.plot(datos_sismograma)
     print("Gráfico generado.")
     plt.show()
 
+def classicSTALTAPy(a, nsta=5*200, nlta=10*200):
+
+    sta = np.cumsum(a ** 2)
+
+
+    sta = np.require(sta, dtype=np.float)
+
+
+    lta = sta.copy()
+
+
+    sta[nsta:] = sta[nsta:] - sta[:-nsta]
+    sta /= nsta
+    lta[nlta:] = lta[nlta:] - lta[:-nlta]
+    lta /= nlta
+
+
+    sta[:nlta - 1] = 0
+
+
+    dtiny = np.finfo(0.0).tiny
+    idx = lta < dtiny
+    lta[idx] = dtiny
+
+    return sta / lta
+
 nombre_archivo = input("Ingrese nombre del archivo (sismograma):\n ")
-opcion = input("Mostrar sismograma (amplitud) = 1 |||| Mostrar sismograma (frecuencias) = 2 |||| Mostrar ruido (aprox.) = 3 \n")
+opcion = input("Mostrar sismograma (amplitud) = 1 |||| Mostrar ruido (aprox.) = 2 |||| Mostrar sismograma (frecuencias) = 3 |||| Mostrar gráfico STA/LTA = 4: \n")
 datos_sismo = obtenerDatos(nombre_archivo)
 
 if(opcion=='1'):
-    mostrarGraficoAmp(datos_sismo)
+    mostrarGrafico(datos_sismo)
 else:
     if (opcion=='2'):
-        datos_transformada = aplicarTransformada(datos_sismo)
-        mostrarGraficoFrec(datos_transformada)
-
+        datos_ruido = obtenerRuido(datos_sismo)
+        mostrarGrafico(datos_ruido)
     else:
-        print("Opci�n inv�lida.")
+        if(opcion=='3'):
+            espectro_frecuencia = espectroFrecuencia(datos_sismo)
+            mostrarGrafico(espectro_frecuencia)
+        else:
+            if(opcion=='4'):
+                sta_lta = classicSTALTAPy(datos_sismo)
+                mostrarGrafico(sta_lta)
+            else:
+                print("Opción inválida.")
