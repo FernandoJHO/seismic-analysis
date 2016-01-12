f507871e13ea958751cff64f5b24e35205c93069
FernandoJHO <fernando.handal@yahoo.com>
2016-01-12 17:32:40 -0300

    modificaciones

--- src/sismograma.py
+++ src/sismograma.py
@@ -1,5 +1,5 @@
 import numpy as np
-import scipy.fftpack as sci
+import scipy as sci
 import matplotlib.pyplot as plt
 
 
@@ -18,23 +18,45 @@ def obtenerRuido(datos):
 
 
 def espectroFrecuencia(datos_sismo):
-    #datos_trans = np.fft.fft(datos_sismo).real
-    datos_trans = np.fft.fft(datos_sismo)
-    datos_trans_freq = np.fft.fftfreq(len(datos_trans),0.5)
-    plt.plot(datos_trans_freq, np.abs(datos_trans))
-    plt.grid()
+    largo = len(datos_sismo)
+    datos_frq = sci.fft(datos_sismo) / largo
+    datos_frq = datos_frq[range(round(largo / 2))]
+
+    return datos_frq
+
+def mostrarGraficoFrecuencia(datos_sismo):
+
+    tasa = 0.5
+    largo = len(datos_sismo)
+    k = sci.arange(largo)
+    t = largo / tasa
+    freq = k / t
+    freq = freq[range(round(largo / 2))]
+
+    datos_frq = sci.fft(datos_sismo) / largo
+    datos_frq = datos_frq[range(round(largo / 2))]
+
+    plt.plot(freq, abs(datos_frq))
     plt.show()
-    #return datos_trans
 
-
-def mostrarGrafico(datos_sismograma):
-    print("Generando gráfico...")
-    plt.plot(datos_sismograma)
-    print("Gráfico generado.")
+def mostrarGraficoTiempo(datos_sismograma):
+    tasa = 0.5
+    t = np.linspace(0, (len(datos_sismograma / tasa)), len(datos_sismograma))
+    plt.plot(t, datos_sismograma)
+    plt.xlabel('Tiempo (s)')
+    plt.ylabel('Amplitud')
     plt.show()
 
+def low_pass_filter(x, samples = 20):
+    a = np.fft.rfft(x)
+    tot = len(a)
+    for x in range(tot-samples):
+        a[samples + x] = 0.0
+    #plt.plot(np.fft.irfft(a))
+    #plt.show()
+    return np.fft.irfft(a)
 
-def classicSTALTAPy(a, nsta=5 * 200, nlta=10 * 200):
+def classicSTALTAPy(a, nsta= 0.5*100, nlta= 0.5*200):
     sta = np.cumsum(a ** 2)
 
     sta = np.require(sta, dtype=np.float)
@@ -54,6 +76,9 @@ def classicSTALTAPy(a, nsta=5 * 200, nlta=10 * 200):
 
     return sta / lta
 
+def graf(datos):
+    plt.plot(datos)
+    plt.show()
 
 nombre_archivo = input("Ingrese nombre del archivo (sismograma):\n ")
 opcion = input(
@@ -61,19 +86,20 @@ opcion = input(
 datos_sismo = obtenerDatos(nombre_archivo)
 
 if opcion == '1':
-    mostrarGrafico(datos_sismo)
+    mostrarGraficoTiempo(datos_sismo)
 else:
     if opcion == '2':
         datos_ruido = obtenerRuido(datos_sismo)
-        mostrarGrafico(datos_ruido)
+        mostrarGraficoTiempo(datos_ruido)
     else:
         if opcion == '3':
-            #espectro_frecuencia =
-            espectroFrecuencia(datos_sismo)
-            #mostrarGrafico(espectro_frecuencia)
+            mostrarGraficoFrecuencia(datos_sismo)
         else:
             if opcion == '4':
-                sta_lta = classicSTALTAPy(datos_sismo)
-                mostrarGrafico(sta_lta)
+                datos_freq = espectroFrecuencia(datos_sismo)
+                datos_filtrados = low_pass_filter(datos_freq)
+                sta_lta = classicSTALTAPy(datos_filtrados)
+                graf(sta_lta)
+                #mostrarGraficoTiempo(sta_lta)
             else:
                 print("Opción inválida.")
